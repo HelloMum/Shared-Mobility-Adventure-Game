368cae5f507384f12bd40c3ca8b634ec39160814
HelloMum <jdg@hotmail.es>
2024-03-13 13:48:10 +0000

    Each element can implement different behavior.

---
+++ src/main/java/HaohaiTeam/Game/Logic/ElementBehavior.java
@@ -0,0 +1,12 @@
+package HaohaiTeam.Game.Logic;
+
+public class ElementBehavior {
+    public ElementBehavior() {
+        // Constructor
+    }
+
+    public void updateBehavior() {
+        // Method to update element behavior
+        System.out.println("Updating element behavior");
+    }
+}
