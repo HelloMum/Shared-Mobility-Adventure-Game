5dc8c031a37d797f575570dd7876d7e92e801592
HelloMum <jdg@hotmail.es>
2024-03-16 23:21:42 +0000

    New classes to create different routes

---
+++ src/main/java/HaohaiTeam/Game/Element/Transport/MarkerStop.java
@@ -0,0 +1,5 @@
+package HaohaiTeam.Game.Element.Transport;
+
+public class MarkerStop {
+    //Buses and taxis stop at markers, taxis can skip markers when destination selected
+}
---
+++ src/main/java/HaohaiTeam/Game/Element/Transport/Road.java
@@ -0,0 +1,5 @@
+package HaohaiTeam.Game.Element.Transport;
+
+public class Road {
+    // Vehicles that are not bikes follow road
+}
