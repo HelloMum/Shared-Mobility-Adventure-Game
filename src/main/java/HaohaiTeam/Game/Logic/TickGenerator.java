e2094b140eb07713a438e72d891c0df8a93422c5
Glenmic <157513318+Glenmic@users.noreply.github.com>
2024-03-31 13:43:34 +0100

    Added initial tick generator design

--- src/main/java/HaohaiTeam/Game/Logic/TickGenerator.java
+++ src/main/java/HaohaiTeam/Game/Logic/TickGenerator.java
@@ -15,33 +15,33 @@ public class TickGenerator {
         this.tickRate = tickRate;
     }
 
-    // Method to start the ticks
+    //Method to start the ticks
     public void start()
-    //method to start ticks - the method calls the onTick method for all listeners in the listener list
+    //Method to start ticks - the method calls the onTick method for all listeners in the listener list
     {
         running = true;
         while (running) {
 
-            Thread.sleep(tickRate); // Wait for the tick rate
+            Thread.sleep(tickRate); //Wait for the tick rate
 
-            // Call 'onTick' method for listeners on each tick
+            //Call 'onTick' method for listeners on each tick
             for (int i = 0; i < listeners.size(); i++) {
                 TickListener listener = listeners.get(i);
                 listener.onTick();
             }
         }
     }
-    // Method to stop the ticks
+    //Method to stop the ticks
     public void stop() {
         running = false;
     }
 
-    // Method to add a listener
+    //Method to add a listener
     public void addTickListener(TickListener listener) {
         listeners.add(listener);
     }
 
-    // Method to remove a listener
+    //Method to remove a listener
     public void removeTickListener(TickListener listener) {
         listeners.remove(listener);
     }
