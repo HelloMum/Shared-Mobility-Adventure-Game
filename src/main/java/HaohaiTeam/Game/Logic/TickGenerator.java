fcc9806830becdac9ff3e8fae98ae7873878101e
Glenmic <157513318+Glenmic@users.noreply.github.com>
2024-03-31 14:15:01 +0100

    Added initial tick generator design

--- src/main/java/HaohaiTeam/Game/Logic/TickGenerator.java
+++ src/main/java/HaohaiTeam/Game/Logic/TickGenerator.java
@@ -3,8 +3,6 @@ package HaohaiTeam.Game.Logic;
 import java.util.ArrayList;
 import java.util.List;
 
-//Implement this interface in a class that is required to have a listener. The 'onTick' method will be called for each
-//implementation of the interface, add to the method in those classes what needs to happen for that class, at a tick.
 public class TickGenerator {
     private boolean running;
     private int tickRate;
