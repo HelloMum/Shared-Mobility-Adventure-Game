a8fc85e370f55609250e12ec8b196f81bb46c85f
Jorge Durán <jdg@hotmail.es>
2024-04-01 23:23:55 +0100

    ElementBehavior.java is discontinued for now if not needed.

--- src/main/java/HaohaiTeam/Game/GameStarter.java
+++ src/main/java/HaohaiTeam/Game/GameStarter.java
@@ -1,15 +1,15 @@
 package HaohaiTeam.Game;
 import HaohaiTeam.Game.GUI.GameWindow;
 import HaohaiTeam.Game.Map.MapLoader;
-import HaohaiTeam.Game.Logic.ElementBehavior;
+// import HaohaiTeam.Game.Logic.ElementBehavior;
 
 public class GameStarter {
 
     public void startGame() { // 之前的startApp现在改名为startGame
         System.out.println("Welcome to Dublin!");
         GameWindow gameWindow = new GameWindow();
-        ElementBehavior elementBehavior = new ElementBehavior();
-        MapLoader mapLoader = new MapLoader(gameWindow, elementBehavior);
+        // ElementBehavior elementBehavior = new ElementBehavior();
+        MapLoader mapLoader = new MapLoader(gameWindow); //, elementBehavior disabled
 
         // 加载起始屏幕，其中包括将元素添加到ElementManager中
         mapLoader.loadMap(MapLoader.level_1);
