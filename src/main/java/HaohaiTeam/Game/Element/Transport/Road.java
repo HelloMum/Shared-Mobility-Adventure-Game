353ce8a89fab715f310983443a57c9b853bfe8fb
Jorge Durán <jdg@hotmail.es>
2024-04-01 23:21:43 +0100

    Road is created and walkable

--- src/main/java/HaohaiTeam/Game/Element/Transport/Road.java
+++ src/main/java/HaohaiTeam/Game/Element/Transport/Road.java
@@ -1,5 +1,24 @@
 package HaohaiTeam.Game.Element.Transport;
 
-public class Road {
-    // Vehicles that are not bikes follow road
+import HaohaiTeam.Game.Element.GameElement;
+import HaohaiTeam.Game.GUI.GameWindow;
+import java.awt.*;
+import java.util.List;
+import static HaohaiTeam.Game.GUI.GameWindow.CELL_SIZE;
+
+import static HaohaiTeam.Game.GUI.GameWindow.CELL_SIZE;
+public class Road extends GameElement {
+    private static final Color ROAD_COLOR = Color.black;
+
+    public Road(int x, int y) {
+        super(x, y);
+        this.walkable = true;
+    }
+
+    @Override
+    public void draw(Graphics2D g2d) {
+        // Draw black background square
+        g2d.setColor(ROAD_COLOR);
+        g2d.fillRect(x, y, CELL_SIZE, CELL_SIZE);
+    }
 }
