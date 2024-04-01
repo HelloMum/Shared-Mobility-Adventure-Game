45301182b01b2068a808abe06592ac294c2db800
Jorge Durán <jdg@hotmail.es>
2024-04-01 05:20:20 +0100

    More cleanup onto the player to re-use more variable and methods

--- src/main/java/HaohaiTeam/Game/Element/Player.java
+++ src/main/java/HaohaiTeam/Game/Element/Player.java
@@ -1,41 +1,28 @@
 package HaohaiTeam.Game.Element;
-import HaohaiTeam.Game.Element.Transport.TransportMode;
 import HaohaiTeam.Game.Logic.currentTransport;
-
 import java.awt.*;
-
 import static HaohaiTeam.Game.GUI.GameWindow.CELL_SIZE;
-
-
 
 public class Player extends GameElement {
     public Player(int x, int y) {
         super(x, y);
     }
-
     public currentTransport state = currentTransport.WALKING;
-
     public boolean beingControlled = true;
 
+    //Setter method currentTransport
     public void setState(currentTransport state) {
         this.state = state;
     }
 
+    //Getter method currentTransport
     public currentTransport getState() {
         return state;
     }
 
-    // Using a means of transportation
-    // This method sets the current transportation mode in use.
-    public void useTransport(currentTransport transport) {
-        this.state = transport;
-    }
-
-    // Cease using the means of transportation, return to walking
     // This method discontinues the use of any transportation and resets it to walking.
     public void stopUsingTransport() {
         this.state = currentTransport.WALKING; // Reset to walking state
-        // Other logic...
         // Update the player's position and visibility as necessary after stopping the transport
     }
 
