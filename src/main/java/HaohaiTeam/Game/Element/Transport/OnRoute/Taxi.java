6e161e3adc7014f3e1eec95776cb07139821af2f
Jorge Durán <jdg@hotmail.es>
2024-04-03 20:35:35 +0100

    Simple taxi for debugging

--- src/main/java/HaohaiTeam/Game/Element/Transport/OnRoute/Taxi.java
+++ src/main/java/HaohaiTeam/Game/Element/Transport/OnRoute/Taxi.java
@@ -24,24 +24,15 @@ public class Taxi extends TransportMode {
     @Override
     public void draw(Graphics2D g2d) {
         g2d.setColor(TAXI_COLOR);
-        // Draw the taxi's body, making it smaller
-        g2d.fillRect(x, y + CELL_SIZE / 4, CELL_SIZE / 2, CELL_SIZE / 2);
+        g2d.fillOval(x, y, CELL_SIZE, CELL_SIZE);
 
-        // Draw the roof of the taxi
-        g2d.fillRect(x + CELL_SIZE / 8, y, CELL_SIZE / 2 - CELL_SIZE / 4, CELL_SIZE / 4);
-
-        // Draw the headlights
-        g2d.setColor(Color.YELLOW);
-        g2d.fillRect(x + CELL_SIZE / 16, y, CELL_SIZE / 8, CELL_SIZE / 8);
-        g2d.fillRect(x + CELL_SIZE / 2 - CELL_SIZE / 8, y, CELL_SIZE / 8, CELL_SIZE / 8);
-
-        // Draw the taxi sign
-        g2d.setColor(Color.BLACK);
-        g2d.drawString("Taxi", x + CELL_SIZE / 8, y + CELL_SIZE / 8);
 
         // Add highlights to the taxi's body for a more realistic effect
         g2d.setColor(Color.WHITE);
-        g2d.drawLine(x + CELL_SIZE / 4, y + CELL_SIZE / 4, x + CELL_SIZE / 2, y); // Body highlight
+        // Draw the taxi sign
+        g2d.setColor(Color.BLACK);
+        g2d.drawString("Taxi", x + CELL_SIZE / 8, y + CELL_SIZE / 2 );
+
     }
 }
 
