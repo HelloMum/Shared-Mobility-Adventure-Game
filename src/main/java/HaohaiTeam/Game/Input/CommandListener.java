4391854c23b4d65d6cc2ac7a4ec6a4a71986d2ad
Jorge Durán <jdg@hotmail.es>
2024-04-03 17:48:18 +0100

    now being used yet

--- src/main/java/HaohaiTeam/Game/Input/CommandListener.java
+++ src/main/java/HaohaiTeam/Game/Input/CommandListener.java
@@ -1,37 +1,37 @@
 package HaohaiTeam.Game.Input;
 
 import java.awt.event.KeyEvent;
-public class CommandListener {
-
-    private CommandSender commandSender;
-    public CommandListener(CommandSender commandSender) {
-        // Constructor
-        this.commandSender = commandSender;
-    }
-
-    public void listenForCommands(KeyEvent e) {
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_UP:
-                commandSender.sendCommands("MOVE_UP");
-                break;
-            case KeyEvent.VK_DOWN:
-                commandSender.sendCommands("MOVE_DOWN");
-                break;
-            case KeyEvent.VK_LEFT:
-                commandSender.sendCommands("MOVE_LEFT");
-                break;
-            case KeyEvent.VK_RIGHT:
-                commandSender.sendCommands("MOVE_RIGHT");
-                break;
-            case KeyEvent.VK_SPACE:
-                commandSender.sendCommands("JUMP");
-                break;
-            default:
-                System.out.println("Unrecognized command");
-        }
-    }
-    public void listenForCommands() {
-        // Method to listen for commands
-        System.out.println("Listening for commands");
-    }
-}
+//public class CommandListener {
+//
+//    private CommandSender commandSender;
+//    public CommandListener(CommandSender commandSender) {
+//        // Constructor
+//        this.commandSender = commandSender;
+//    }
+//
+//    public void listenForCommands(KeyEvent e) {
+//        switch (e.getKeyCode()) {
+//            case KeyEvent.VK_UP:
+//                commandSender.sendCommands("MOVE_UP");
+//                break;
+//            case KeyEvent.VK_DOWN:
+//                commandSender.sendCommands("MOVE_DOWN");
+//                break;
+//            case KeyEvent.VK_LEFT:
+//                commandSender.sendCommands("MOVE_LEFT");
+//                break;
+//            case KeyEvent.VK_RIGHT:
+//                commandSender.sendCommands("MOVE_RIGHT");
+//                break;
+//            case KeyEvent.VK_SPACE:
+//                commandSender.sendCommands("JUMP");
+//                break;
+//            default:
+//                System.out.println("Unrecognized command");
+//        }
+//    }
+//    public void listenForCommands() {
+//        // Method to listen for commands
+//        System.out.println("Listening for commands");
+//    }
+//}
--- src/main/java/HaohaiTeam/Game/Input/InputHandler.java
+++ src/main/java/HaohaiTeam/Game/Input/InputHandler.java
@@ -2,22 +2,22 @@ package HaohaiTeam.Game.Input;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-public class InputHandler extends KeyAdapter{
-
-    private CommandListener commandListener;
-
-    public InputHandler(CommandListener commandListener) {
-        // Constructor
-        this.commandListener = commandListener;
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        commandListener.listenForCommands(e);
-    }
-
-    public void handleInput() {
-        // Method to handle input
-        System.out.println("Handling input");
-    }
-}
+//public class InputHandler extends KeyAdapter{
+//
+//    private CommandListener commandListener;
+//
+//    public InputHandler(CommandListener commandListener) {
+//        // Constructor
+//        this.commandListener = commandListener;
+//    }
+//
+//    @Override
+//    public void keyPressed(KeyEvent e) {
+//        commandListener.listenForCommands(e);
+//    }
+//
+//    public void handleInput() {
+//        // Method to handle input
+//        System.out.println("Handling input");
+//    }
+//}
