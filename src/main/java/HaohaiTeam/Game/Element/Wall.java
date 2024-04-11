312cba4c39e559302576cc6e5526b362ebf824a0
Jorge Durán <jdg@hotmail.es>
2024-04-11 23:35:10 +0100

    Not needed anymore, just for debug

--- src/main/java/HaohaiTeam/Game/Element/Wall.java
+++ src/main/java/HaohaiTeam/Game/Element/Wall.java
@@ -24,10 +24,6 @@ public class Wall extends GameElement {
     @Override
     public void onBeingCollidedOnYou(GameElement gameElement) {
         System.out.println(this + " collision on the element " + gameElement);
-        if (gameElement instanceof Player) {
-            // If the collision is with a Wall, show a pop-up indicating inability to walk through walls
-            new PopUp(this.X, this.Y,"You cannot walk through walls");
-        }
         // Notify the other element about the collision if needed
         gameElement.onBeingCollidedOnYou(this);
     }
