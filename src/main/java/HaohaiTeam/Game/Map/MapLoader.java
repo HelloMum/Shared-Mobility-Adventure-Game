3bb237fbb0acec8ce911671e24458c810efb1d4a
Jorge Durán <jdg@hotmail.es>
2024-04-11 22:14:09 +0100

    Multiples wagons of luas disables until properly implemented.

--- src/main/java/HaohaiTeam/Game/Map/MapLoader.java
+++ src/main/java/HaohaiTeam/Game/Map/MapLoader.java
@@ -22,7 +22,7 @@ public class MapLoader {
     public static String[] level_1 = {
             "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
             "W  rrrrrrrrr                   W",
-            "W  r P  W Cr     C    LLL   B  W",
+            "W  r P  W Cr     C    L     B  W",
             "W  r    W  c WWWWWW  rrrrrrr   W",
             "W  r    W  r WGGGGW  r     r   W",
             "W  r    W  r WGGGG   rrrlrrr   W",
