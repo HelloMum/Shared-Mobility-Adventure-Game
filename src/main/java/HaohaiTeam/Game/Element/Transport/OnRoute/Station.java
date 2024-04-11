2eb402496d21ff2ea1de4008cf294f87d7217d54
Jorge Durán <jdg@hotmail.es>
2024-04-11 05:00:59 +0100

    These two interactions should be a good start

--- src/main/java/HaohaiTeam/Game/Element/Transport/OnRoute/Station.java
+++ src/main/java/HaohaiTeam/Game/Element/Transport/OnRoute/Station.java
@@ -1,5 +1,7 @@
 package HaohaiTeam.Game.Element.Transport.OnRoute;
 
+import HaohaiTeam.Game.Element.GameElement;
+
 import java.util.Set;
 
 public abstract class Station extends Road {
@@ -7,5 +9,12 @@ public abstract class Station extends Road {
     public Station(int x, int y) {
         super(x, y);
         this.walkable = true;
+    }
+    @Override
+    public void interactKeyPressedOnYou(GameElement gameElement) {
+    }       // If player wants to interact and bus linked to station give move player onto the bus and link to it
+    public void goingToBeWalkedOverBy(GameElement gameElement) {
+        // If this will be override by subclass with their elements if bus on top stop move of bus for 3 seconds and keep the vehicle instance in case player wants  to get on bus,
+        // if player wants to get on bus move player take out control until next station
     }
 }
