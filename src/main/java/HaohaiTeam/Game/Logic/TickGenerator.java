20b4dcacf5606e94b4e238017c6f5c6f510d2b80
Jorge Durán <jdg@hotmail.es>
2024-04-11 05:47:20 +0100

    Tick fixes

--- src/main/java/HaohaiTeam/Game/Logic/TickGenerator.java
+++ src/main/java/HaohaiTeam/Game/Logic/TickGenerator.java
@@ -1,13 +1,12 @@
 package HaohaiTeam.Game.Logic;
 
-import HaohaiTeam.Game.Element.GameElement;
 import HaohaiTeam.Game.Input.CommandListener;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class TickGenerator implements CommandListener {
+public class TickGenerator {
     private static ScheduledExecutorService executor;
     private static int tickRate;
     private static CommandListener commandListener;
@@ -15,15 +14,12 @@ public class TickGenerator implements CommandListener {
     public TickGenerator() {
         tickRate = 100;
         executor = Executors.newSingleThreadScheduledExecutor(); // Create a thread for tick time tracking
-        System.out.println("Tic");
-
     }
 
-    public void start() {
-        System.out.println("Start");
+    public static void start() {
         executor.scheduleAtFixedRate(() -> {
             if (commandListener != null) {
-                this.onTick();
+                commandListener.onTick();
             }
         }, 0, tickRate, TimeUnit.MILLISECONDS);
     }
@@ -32,44 +28,16 @@ public class TickGenerator implements CommandListener {
         TickGenerator.commandListener = commandListener;
     }
 
-    public static void stop() {
+    public void stop() {
         executor.shutdown();
     }
 
-    public static void addTickListener(Runnable listener) {
+    public void addTickListener(Runnable listener) {
         // Implement if needed
     }
 
-    public static void changeTickRate(int tickRate) {
-        TickGenerator.tickRate = tickRate;
-    }
 
-    public static void removeTickListener(Runnable listener) {
+    public void removeTickListener(Runnable listener) {
         // Implement if needed
-    }
-
-    @Override
-    public void onPickedCoin(GameElement element) {
-
-    }
-
-    @Override
-    public void onPickedGem(GameElement element) {
-
-    }
-
-    @Override
-    public void onTransportEntered(GameElement element) {
-
-    }
-
-    @Override
-    public void onTransportExited(GameElement element) {
-
-    }
-
-    @Override
-    public void onTick() {
-
     }
 }
