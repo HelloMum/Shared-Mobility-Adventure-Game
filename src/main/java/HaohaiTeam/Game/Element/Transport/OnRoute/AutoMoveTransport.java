66f01ab38ae786e3977dc8b134edaea05f0f3cd5
Jorge Durán <jdg@hotmail.es>
2024-04-11 05:48:50 +0100

    Rolled back the changes but this should be using the tick

--- src/main/java/HaohaiTeam/Game/Element/Transport/OnRoute/AutoMoveTransport.java
+++ src/main/java/HaohaiTeam/Game/Element/Transport/OnRoute/AutoMoveTransport.java
@@ -1,59 +1,97 @@
 package HaohaiTeam.Game.Element.Transport.OnRoute;
 
 import HaohaiTeam.Game.Element.GameElement;
+import HaohaiTeam.Game.Element.Transport.TransportMode;
 import HaohaiTeam.Game.GUI.GameWindow;
+
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public abstract class AutoMoveTransport extends GameElement {
-    public boolean moving; // This refers if moving
+import static HaohaiTeam.Game.GUI.GameWindow.CELL_SIZE;
+/**
+ * An abstract base class for transport modes that automatically move along roads.
+ */
+public abstract class AutoMoveTransport extends TransportMode {
+    protected Color color;
+    protected static final int MOVE_INTERVAL_MS = 100; // Move every 1 second
 
-    @Override
-    public void onTick() {
-        tickCount++;
-        System.out.println("Tic");
-        // Move every 5 ticks
-        if (tickCount % 5 == 0) {
-            moveOnRoad(this.getLogicalPosX(), this.getLogicalPosY()); // Start moving on road
-            System.out.println("Tic");
+    // HeadingX and HeadingY: initially point to the lower right (due east-south),
+    // indicating the current direction of parking.
+    protected int headingX = 0;
+    protected int headingY = 1;
 
-
-        }
-    }
     public AutoMoveTransport(int x, int y) {
-        super(x,y);
-        moving = true;
-        layer = 101; // Above player and road
+        super(x, y);
+        startMoving();
     }
 
-    private void moveOnRoad(int onX, int onY) {
-        while (moving) {
-            tryMoveInCurrentDirection(onX, onY);
-        }
+    protected void startMoving() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                moveOnRoad();
+            }
+        }, 0, MOVE_INTERVAL_MS);
     }
 
-    private void tryMoveInCurrentDirection(int onX, int onY) {
+    protected void moveOnRoad() {
         List<GameElement> elements = new ArrayList<>(GameWindow.getElements());
 
-        // Calculate the new position based on the current position and direction
-        int newX = this.getLogicalPosX() + onX;
-        int newY = this.getLogicalPosY() + onY;
+        // Try to move in the current direction first
+        if (tryMoveInCurrentDirection(elements)) return;
 
-        // Check if the new position is valid and free from obstacles
-        if (isValidMove(newX, newY, elements)) {
-            // Move to the new position
-            this.setToLogicalPos(newX, newY);
-        }
+        // If moving in the current direction is not possible, try to turn right
+        if (tryTurn(elements, headingY, -headingX)) return;
+
+        // If turning right is not successful, try to turn left
+        if (tryTurn(elements, -headingY, headingX)) return;
+
+        // If turning left is also not successful, try to turn around (180 degrees)
+        tryTurn(elements, -headingX, -headingY);
     }
 
-    // Check if the move to the new position is valid
-    private boolean isValidMove(int newX, int newY, List<GameElement> elements) {
-        // Check if the new position is occupied by any obstacles
+    private boolean tryMoveInCurrentDirection(List<GameElement> elements) {
+        int nextX = X + headingX * CELL_SIZE;
+        int nextY = Y + headingY * CELL_SIZE;
+        if (isRoadAtPosition(nextX, nextY, elements)) {
+            logicalMove(headingX, headingY);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean tryTurn(List<GameElement> elements, int dx, int dy) {
+        int nextX = X + dx * CELL_SIZE;
+        int nextY = Y + dy * CELL_SIZE;
+        if (isRoadAtPosition(nextX, nextY, elements)) {
+            updateHeading(dx, dy);
+            logicalMove(dx, dy);
+            return true;
+        }
+        return false;
+    }
+
+
+    protected boolean isRoadAtPosition(int x, int y, List<GameElement> elements) {
+
+        // Check if there is a road at the specified position
+        // This method typically does not need to be overridden by subclasses
+
         for (GameElement element : elements) {
-            if (element.getLogicalPosX() == newX && element.getLogicalPosY() == newY && !(element instanceof Road)) {
-                return false;
+            if (element instanceof Road && element.X == x && element.Y == y) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
+
+    protected void updateHeading(int dx, int dy) {
+        headingX = dx;
+        headingY = dy;
+    }
+
 }
