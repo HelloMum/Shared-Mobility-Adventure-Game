af20ee1274550566a54e9bd93f5a0e4c29521afc
Evan <evan.ucd@gmail.com>
2024-04-18 17:47:40 +0100

    fix link logic in station

--- src/main/java/haohaiTeam/game/element/transport/onRoute/stationRoad/Station.java
+++ src/main/java/haohaiTeam/game/element/transport/onRoute/stationRoad/Station.java
@@ -63,7 +63,7 @@ public abstract class Station extends Road {
         AutoMoveTransport real_vehicle = findVehicleAtStation();
         if (real_vehicle != null && real_vehicle.isAtStation()) {
             System.out.println("Linking player to " + real_vehicle.getName());
-            gameElement.linkElement(real_vehicle);  // Link player to the vehicle
+            real_vehicle.linkElement(gameElement);  // Link player to the vehicle
             real_vehicle.setBeingControlled(false);  // The vehicle should auto-move
             gameElement.setBeingControlled(false);
             System.out.println("Player is now on board the " + real_vehicle.getClass().getSimpleName());
