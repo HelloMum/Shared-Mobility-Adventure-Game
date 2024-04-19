139498ccf8173c490ae65c65b14745b7dc2aa21c
Evan <evan.ucd@gmail.com>
2024-04-19 18:49:56 +0100

    I added some comments for the logic of getting on vehicles it is nice now and I deleted the codes of fakeVehicles coz now I didnt use them

--- src/main/java/haohaiTeam/game/element/transport/onRoute/stationRoad/Station.java
+++ src/main/java/haohaiTeam/game/element/transport/onRoute/stationRoad/Station.java
@@ -4,14 +4,9 @@ import haohaiTeam.game.element.GameElement;
 import haohaiTeam.game.element.Player;
 import haohaiTeam.game.element.PopUp;
 import haohaiTeam.game.element.transport.onRoute.auto.AutoMoveTransport;
-import haohaiTeam.game.element.transport.onRoute.faketrans.FakeBus;
-import haohaiTeam.game.element.transport.onRoute.faketrans.FakeLuas;
-import haohaiTeam.game.element.transport.onRoute.faketrans.FakeTaxi;
-import haohaiTeam.game.element.transport.onRoute.faketrans.FakeVehicle;
 import haohaiTeam.game.gui.GameWindow;
 
 import java.util.List;
-import java.util.HashMap;
 
 public abstract class Station extends Road {
     protected char stationType;// this is an identifier for station
@@ -24,22 +19,6 @@ public abstract class Station extends Road {
     @Override
     public void interactKeyPressedOnYou(GameElement gameElement) {
         // If player wants to interact and bus linked to station give move player onto the bus and link to it
-
-        if (gameElement instanceof Player) {
-            FakeVehicle vehicle;
-            if (this instanceof BusStation) {
-                vehicle = new FakeBus(this.X, this.Y);
-            } else if (this instanceof LuasStation) {
-                vehicle = new FakeLuas(this.X, this.Y);
-            } else if (this instanceof TaxiStation) {
-                vehicle = new FakeTaxi(this.X, this.Y);
-            } else {
-                return;
-            }
-            vehicle.initiateMovement(this);
-            System.out.println("key-------successfully");
-
-        }
     }
     @Override
     public void onBeingCollidedOnYou(GameElement gameElement) {
@@ -63,13 +42,27 @@ public abstract class Station extends Road {
 
 
         // Check if the player is on any vehicle and handle accordingly
+        // Find an available vehicle at the station
         AutoMoveTransport real_vehicle = findVehicleAtStation();
+
+        // If a vehicle is found at the station and it is currently at the station
         if (real_vehicle != null && real_vehicle.isAtStation()) {
+            // Link the game element to the vehicle
             real_vehicle.linkElement(gameElement);
+
+            // Link the vehicle to the game element
             gameElement.linkElement(real_vehicle);
+
+            // Move the game element to the linked vehicle's position
             gameElement.moveToLinked();
+
+            // Set the vehicle's being controlled status to false
             real_vehicle.setBeingControlled(false);
+
+            // Set the game element's being controlled status to false
             gameElement.setBeingControlled(false);
+
+            // Print a message indicating that the player is now on board the vehicle
             System.out.println("Player is now on board the " + real_vehicle.getClass().getSimpleName());
         }
     }
