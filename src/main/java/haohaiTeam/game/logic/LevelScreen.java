0c287ff96009eac0c2a014c773d98e7e0578de87
Evan <evan.ucd@gmail.com>
2024-04-22 15:14:50 +0100

    here in LevelScreen I called loadNextLevel() when the player wins and should reload current level again but now we dont implement 'reload'

--- src/main/java/haohaiTeam/game/logic/LevelScreen.java
+++ src/main/java/haohaiTeam/game/logic/LevelScreen.java
@@ -2,6 +2,8 @@ package haohaiTeam.game.logic;
 
 import java.awt.*;
 import haohaiTeam.game.gui.GameWindow;
+import haohaiTeam.game.map.MapLoader;
+
 public class LevelScreen {
     private GameStatus gameStatus;
     private int width;
@@ -15,44 +17,48 @@ public class LevelScreen {
         System.out.println("Level Screen Created");
         System.out.println("-------------------------");
     }
-    
+
     public void render(Graphics g) {
         if (gameStatus.isGameOver() == true || this.gameStatus.winningCondition() == true) {
         // rectangle
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, width, height);
-        
+
         // text
         g.setColor(Color.BLACK);
         g.setFont(new Font("Arial", Font.BOLD, 20));
         String gameOverMessage = "Game Over!";
         String winMessage = "Level finished!";
         int messageWidth = g.getFontMetrics().stringWidth(gameOverMessage);
-        int x = (width - messageWidth) / 2; // center the text 
-        
+        int x = (width - messageWidth) / 2; // center the text
+
         int score = gameStatus.getScore();
         int gems = gameStatus.getGemsAcquired();
         int coins = gameStatus.getCoinsCollected();
         int c02 = gameStatus.getCO2Collected();
-        
+
         g.drawString("Score: " + score, 150, 200);
         g.drawString("Gems: " + gems, 150, 230);
         g.drawString("Coins: " + coins, 150, 260);
         g.drawString("C02: " + c02, 150, 290);
-        
+
         //Enviroventure at the bottom:
         String enviroVentureText = "EnviroVenture";
         int enviroVentureWidth = g.getFontMetrics().stringWidth(enviroVentureText);
         int enviroVentureX = (width - enviroVentureWidth) / 2;
         int enviroVentureY = height - 50;
         g.drawString(enviroVentureText, enviroVentureX, enviroVentureY);
-    
+
         if (this.gameStatus.isGameOver() == true) {
             g.drawString(gameOverMessage, x, 50);
+            // reload current level
         }
         if (this.gameStatus.winningCondition() == true) {
             g.drawString(winMessage, x, 50);
+            // sleep some time and continue the game next level
+            // load next level
+            MapLoader.loadNextLevel();
         }
-    }    
+    }
 }
 }
