0f9ca30fc68f38095dbccb8ab958712b09617da2
Evan <evan.ucd@gmail.com>
2024-04-23 22:36:44 +0100

    reorganized the sequence and order of the codes, please use clear order:variables-constructor-getter and setter-other methods. and please note if you will use the method later, you should put this method before other codes that will call this method

--- src/main/java/haohaiTeam/game/logic/GameStatus.java
+++ src/main/java/haohaiTeam/game/logic/GameStatus.java
@@ -7,7 +7,7 @@ import haohaiTeam.game.input.CommandListener;
 
 public class GameStatus implements CommandListener {
     private int score = 0;
-    private final int lives = 3; // assume 3 lives at start
+    private static final int lives = 3; // assume 3 lives at start
     private int coinsCollected = 0;
     private int gemsAcquired = 0;
     private int co2Collected = 0;
@@ -20,15 +20,21 @@ public class GameStatus implements CommandListener {
     public static boolean co2increase = false;
     private static final int REQUIRED_GEMS = 20; // Number of gems required to win
     private boolean gameWon = false;
+
     // here I am not sure there is a need to have a timer for co2 but this one is for checking game time
     private static final long TIME_LIMIT_IN_SECONDS = 60; // this is for limiting the player to pass current level in 60 seconds
+
+    public GameStatus() {
+        // Initialize the CO2 timer
+        gameTimer();
+    }
 
     public void addScore(int points) {
         this.score += points;
     }
-    public GameStatus() {
-        // Initialize the CO2 timer
-        gameTimer();
+
+    public int getScore() {
+        return score;
     }
 
     public boolean isGameWon() {
@@ -38,6 +44,15 @@ public class GameStatus implements CommandListener {
     public void setGameWon(boolean gameWon) {
         this.gameWon = gameWon;
     }
+
+    public void setGameOver(boolean gameOver) {
+        this.gameOver = gameOver;
+    }
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+
     public void gameTimer() {
         // Initialize the timer
         timer = new Timer();
@@ -57,16 +72,26 @@ public class GameStatus implements CommandListener {
             }
         }, TIMER_DELAY, TIMER_DELAY);
     }
-    public int getScore() {
-        return score;
+
+    // Method to update elapsed time
+    public void updateElapsedTime(long elapsedTimeInSeconds) {
+        this.elapsedTimeInSeconds += elapsedTimeInSeconds;
+        // check if time limit is exceeded
+        if (this.elapsedTimeInSeconds > TIME_LIMIT_IN_SECONDS) {
+            System.out.println("Time's up! Game over!");
+            setGameOver(true);
+        }
     }
 
-
-    public void setGameOver(boolean gameOver) {
-        this.gameOver = gameOver;
+    public void trackCO2Level() {
+        if (this.getCO2Collected() > MAX_CO2_LEVEL) {
+            this.setGameOver(true);
+            System.out.println("CO2 level exceeded maximum amount. Game Over!");
+        }
     }
-    public boolean isGameOver() {
-        return gameOver;
+
+    public void increaseCO2() {
+        this.co2Collected++;
     }
 
     public void addCoins(int numCoins) {
@@ -84,26 +109,6 @@ public class GameStatus implements CommandListener {
         addScore(points);
     }
 
-    public void trackCO2Level() {
-        if (this.getCO2Collected() > MAX_CO2_LEVEL) {
-            this.setGameOver(true);
-            System.out.println("CO2 level exceeded maximum amount. Game Over!");
-        }
-    }
-    // Method to update elapsed time
-    public void updateElapsedTime(long elapsedTimeInSeconds) {
-        this.elapsedTimeInSeconds += elapsedTimeInSeconds;
-        // check if time limit is exceeded
-        if (this.elapsedTimeInSeconds > TIME_LIMIT_IN_SECONDS) {
-            System.out.println("Time's up! Game over!");
-            setGameOver(true);
-        }
-    }
-
-    public void increaseCO2() {
-        this.co2Collected++;
-    }
-
     public int getCoinsCollected() {
         return coinsCollected;
     }
@@ -114,6 +119,17 @@ public class GameStatus implements CommandListener {
 
     public int getCO2Collected() {
         return co2Collected;
+    }
+
+    public void checkGameConditions() {
+        if (lives <= 0 || elapsedTimeInSeconds > TIME_LIMIT_IN_SECONDS || co2Collected > MAX_CO2_LEVEL) {
+            gameOver = true;
+            System.out.println("Game Over! You have lost.");
+        } else if (gemsAcquired >= REQUIRED_GEMS && co2Collected <= MAX_CO2_LEVEL && elapsedTimeInSeconds <= TIME_LIMIT_IN_SECONDS) {
+            gameOver = true;
+            setGameWon(true);
+            System.out.println("Congratulations! You have won.");
+        }
     }
 
     @Override
@@ -130,16 +146,6 @@ public class GameStatus implements CommandListener {
         checkGameConditions();
     }
 
-    public void checkGameConditions() {
-        if (lives <= 0 || elapsedTimeInSeconds > TIME_LIMIT_IN_SECONDS || co2Collected > MAX_CO2_LEVEL) {
-            gameOver = true;
-            System.out.println("Game Over! You have lost.");
-        } else if (gemsAcquired >= REQUIRED_GEMS && co2Collected <= MAX_CO2_LEVEL && elapsedTimeInSeconds <= TIME_LIMIT_IN_SECONDS) {
-            gameOver = true;
-            setGameWon(true);
-            System.out.println("Congratulations! You have won.");
-        }
-    }
     @Override
     public void onTick() {
         tickCount++;
