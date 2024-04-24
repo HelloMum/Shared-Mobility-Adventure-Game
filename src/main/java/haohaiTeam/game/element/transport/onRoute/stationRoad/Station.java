4f575cec6ceb238be0da977354615e842dbb80aa
Evan <evan.ucd@gmail.com>
2024-04-24 17:30:59 +0100

    when player get on vehicels call onCO2Generated and I changed CO2_PER_CELL as int

--- src/main/java/haohaiTeam/game/element/transport/onRoute/stationRoad/Station.java
+++ src/main/java/haohaiTeam/game/element/transport/onRoute/stationRoad/Station.java
@@ -4,13 +4,14 @@ import haohaiTeam.game.element.GameElement;
 import haohaiTeam.game.element.Player;
 import haohaiTeam.game.element.transport.onRoute.auto.AutoMoveTransport;
 
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 public abstract class Station extends Road {
     public AutoMoveTransport transportReference; // Reference to the transport
-
-    public static final double CO2_PER_CELL = 0;
+//    CommandListener commandListener;
+    public static final int CO2_PER_CELL = 0;
     public int distanceNext = 0;
 
     public Station(int x, int y) {
@@ -65,6 +66,7 @@ public abstract class Station extends Road {
                 // if player linked with vehicles, we should add co2 as we know from current station
                 // here use getDistanceNextStation() * CO2_PER_CELL to calculate co2
                 // record the trips and vehicles that the user used as we need to implement pop-up environment impact
+                this.commandListener.onCO2Generated((int)this.getDistanceNextStation() * CO2_PER_CELL);
             }
         }
     }
