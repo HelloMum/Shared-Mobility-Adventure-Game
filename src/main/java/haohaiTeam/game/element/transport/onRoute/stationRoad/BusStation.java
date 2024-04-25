11bf897b96e4f3837a3a640a7dea79eecc22e08d
Jorge Durán <jdg@hotmail.es>
2024-04-25 02:36:02 +0100

    Missing bus class, added import.

--- src/main/java/haohaiTeam/game/element/transport/onRoute/stationRoad/BusStation.java
+++ src/main/java/haohaiTeam/game/element/transport/onRoute/stationRoad/BusStation.java
@@ -1,5 +1,8 @@
 package haohaiTeam.game.element.transport.onRoute.stationRoad;
 
+import haohaiTeam.game.element.GameElement;
+import haohaiTeam.game.element.transport.onRoute.auto.Bus;
+
 import java.awt.*;
 
 import static haohaiTeam.game.gui.GameWindow.CELL_SIZE;
